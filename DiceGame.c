0ea27cedfb27a1cb7d680220ddff419306326120
s1300002 <s1300002@u-aizu.ac.jp>
2023-06-12 15:00:57 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -4,7 +4,8 @@
 
 int main() {
     int num1, num2, sum;
-    
+    char name[10];
+      
     srand(time(NULL));
     
     num1 = rand() % 6 + 1;
@@ -12,10 +13,13 @@ int main() {
     
     sum = num1 + num2;
 
+    printf("What your name?\n");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
     printf("Rolling dice...\n");
     printf("Die 1: %d\n", num1);
     printf("Die 2: %d\n", num2);
-    printf("Total value: %d\n", sum);
+    printf("Total value: %d\n", sum);    
     
     return 0;
 }
